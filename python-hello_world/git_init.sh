ce25441f0b6978136e44d8e3598d4f1b727905a5
Ahmed <151867276+Ahmedsawan42@users.noreply.github.com>
2026-04-23 18:18:28 +0200

    Delete python-hello_world/git_init.sh

--- python-hello_world/git_init.sh
+++
@@ -1,5 +0,0 @@
-git init
-git remote add origin https://github.com/Ahmedsawan42/dlh-higher_level_programming.git
-git config --global user.name "Ahmedsawan42"
-git config --global user.email "ahmed42lux@gmail.com"
-git config --global credential.helper 'cache --timeout=7200'
