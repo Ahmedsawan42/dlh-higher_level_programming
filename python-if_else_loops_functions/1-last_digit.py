35c61ebe9e4046c4cad19d7046256ad1eccd46c1
Ahmedsawan42 <ahmed42lux@gmail.com>
2026-04-25 12:49:46 +0000

    mod task1, 8 and 9

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str = str(number)
-last = str[-1]
-last_num = int(last)
-if int(last) > 5 and number > 0:
-    print(f"Last digit of {number} is {last_num} and is greater than 5")
-elif int(last) == 0:
-    print(f"Last digit of {number} is {last_num} and is 0")
-elif number > 0:
-    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
+
+strng = str(number)
+last_chr = strng[-1]
+last = int(last_chr)
+
+if number < 0 and last != 0:
+    last = -last
+
+if last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
 else:
-    last_num = last_num * -1
-    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if isinstance(number, (int, float)):
-        text = str(number)
-        print(text[-1], end="")
-        return (text[-1])
+
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
